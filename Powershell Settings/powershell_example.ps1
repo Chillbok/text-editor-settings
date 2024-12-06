b9fcdfd7ceebcddf41651ca8de204f4c423a41b2
chillbok <kylejunho@gmail.com>
2024-12-06 11:47:50 +0900

    Auto-Commited From Windows 11

--- Powershell Settings/powershell_example.ps1
+++ Powershell Settings/powershell_example.ps1
@@ -1,7 +1,24 @@
 oh-my-posh init pwsh | Invoke-Expression
 neofetch
 
-function Logseq-Directory {
+# Reload-*: 특정 파일 다시 로딩
+# Modify-*: 특정 파일 수정
+# Cd-*: 특정 파일로 이동
+# Macro-*: 명령어 모음집 실행
+
+function Modify-Vimrc { # _vimrc 수정하기
+	vim "C:\Program Files\Vim\_vimrc"
+}
+
+function Modify-PROFILE { # $PROFILE 수정하기
+	vim $PROFILE
+}
+
+function Reload-PROFILE { # 프로필 다시 로딩
+	. $PROFILE
+}
+
+function Cd-LogseqDirectory { # Logseq github 리포지토리 열고, git 상태 확인
 	cd $HOME\Repositories\logseq-zettelkasten\
 
 	$fetchResult = git fetch 2>&1 # git fetch의 결과를 변수에 저장
@@ -17,16 +34,12 @@ function Logseq-Directory {
 	echo $status # git status 출력
 }
 
-function Repositories {
+function Cd-Repositories { # github 리포지토리 폴더 열기
 	cd $HOME\Repositories
 }
 
-function Auto-Commit {
+function Macro-Commit { # 자동으로 commit하고 push까지 시키는 명령어
 	git add *
 	git commit -m "Auto-Commited From Windows 11"
 	git push
-}
-
-function Reload-Profile {
-	. $PROFILE
 }
